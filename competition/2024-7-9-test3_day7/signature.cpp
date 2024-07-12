b1f4b56e9c0a076dbb7ee1faf8b3e4f44848640f
big_quantum <1959168878@qq.com>
2024-07-13 07:56:00 +0800

    202407130755

---
+++ competition/2024-7-9-test3_day7/signature.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int c, n, s[205], t[205];
+
+int main(){
+    freopen("signature.in", "r", stdin);
+    freopen("signature.out", "w", stdout);
+    cin >> c >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> s[i];
+    }
+    for(int i = 1; i <= n; i++){
+        cin >> t[i];
+    }
+    cout << 0 << endl;
+    return 0;
+}
